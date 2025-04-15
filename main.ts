b3bb9b8ad099671cd77f318a3e41210603787261
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-04-15 11:27:20 +0200

    Removed clutter

--- main.ts
+++ main.ts
@@ -1,27 +1,12 @@
 import { Hono } from '@hono/hono'
 import { logger } from '@hono/hono/logger'
 import { startUpDatabase } from "./database/config.ts"
-import { calculateJetsamCost } from './utilities.ts'
 import canal from "./canal/canal.routes.ts"
 import payments from "./payments/payments.routes.ts"
 
 const app = new Hono()
 startUpDatabase()
-// const bytes = 1024*1024*1024*1024
-// console.log(calculateJetsamCost(bytes, 1))
-// const wordstext = await Deno.readTextFile('wordlist.txt')
-// const wordsarray = wordstext.split('\n')
-// const words: { [key: string]: object } = {}
-// wordsarray.forEach((word) => {
-//   const key = word.split('\t')[0]
-//   const value = word.split('\t')[1]
-//   words[key] = {
-//     "original_word": value,
-//     "mirror_word": "",
-//     "relationship": ""
-//   }
-// })
-// await Deno.writeTextFile('./spoilerlistb.json', JSON.stringify(words, null, 2)) 
+
 app.use(logger())
 app.get('/', c => c.text('Welcome to the world of spies'))
 app.route('/canal', canal)
