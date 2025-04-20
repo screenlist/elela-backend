869f47013c51b67ad74e8c39f306329dfd638e81
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-04-20 23:06:36 +0200

    Created Jetsam WIP

---
+++ jetsam/jetsam.config.ts
@@ -0,0 +1,23 @@
+import { RecordId } from "@surrealdb/surrealdb";
+
+export type Cargo = {
+  id: RecordId<string>
+  b2_file_id: string
+  points: number
+  downloads: number
+  name: string
+  content_type: string
+  size: number
+  upload_complete: boolean
+  is_independent: boolean
+  is_public: boolean
+  public_code?: string
+  flookey_active_from: Date
+  storage_valid_until: Date
+  created_at: Date
+  updated_at: Date
+}
+
+export type UploadSession = {
+  id: RecordId<string>
+}
---
+++ jetsam/jetsam.routes.ts
@@ -0,0 +1,41 @@
+import { Hono } from '@hono/hono'
+import { HTTPException } from '@hono/hono/http-exception'
+import { RecordId, surql } from "@surrealdb/surrealdb"
+import { getSurreal } from "../database/config.ts";
+import { calculateJetsamCost, verifyRequest } from "../utilities.ts";
+
+const db = await getSurreal()
+const bucket = Deno.env.get('BB_BUCKET')
+const jetsam = new Hono<{ Variables: {user: {id: string, table: string}} }>()
+
+jetsam.post('/bridge', async c => {
+  try {
+    const name = crypto.randomUUID()
+    const body = await c.req.formData()
+    const file = body.get('pic') as File
+
+    const url = Deno.env.get('APP_ENV') === 'production' ? `https://${Deno.env.get('HOST')}/jetsam/bridge/${name}` : `https://f003.backblazeb2.com/file/${bucket}/${name}`
+    return c.json({url})
+  } catch (error) {
+    console.log(error)
+    throw new HTTPException(400, { message: 'Could not upload', cause: error })
+  }
+})
+
+jetsam.post('/cost', c => {
+  const size = c.req.query('size')
+  const downloads = c.req.query('downloads') ? Number(c.req.query('downloads')) : 3
+  const retention = c.req.query('retention') ? Number(c.req.query('retention')) : 1
+
+  if(!size){ throw new HTTPException(404, { message: 'File size must be proived' }) }
+
+  if(typeof Number(size) !== 'number'){ throw new HTTPException(404, { message: 'File size must be a number' }) }
+
+  return c.json(calculateJetsamCost(+size, downloads, retention))
+})
+
+jetsam.post('/start', verifyRequest(['sailor']), async c => {
+  
+})
+
+export default jetsam
