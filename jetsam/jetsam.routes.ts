fb2ee2b4b50f5334d643653c2aadb5e8ea4ac2e5
Alex Kokobane <63163413+alexkokobane@users.noreply.github.com>
2025-06-15 22:14:21 +0200

    Tested out the backblaze api

--- jetsam/jetsam.routes.ts
+++ jetsam/jetsam.routes.ts
@@ -3,9 +3,18 @@ import { HTTPException } from '@hono/hono/http-exception'
 import { RecordId, surql } from "@surrealdb/surrealdb"
 import { db } from "../database/config.ts";
 import { Billing, verifyRequest } from "../utilities.ts";
+import { encodeBase64 } from "@std/encoding";
 
 const billing = new Billing()
 const bucket = Deno.env.get('BB_BUCKET')
+const endpoint = Deno.env.get('BB_ENDPOINT')
+const keys = () => {
+  const id = Deno.env.get('BB_KEY_ID')
+  const key = Deno.env.get('BB_KEY')
+  if(!id || !key){ throw new HTTPException(400, { message: 'The object storage cannot be reached' }) }
+
+  return encodeBase64(`${id}:${key}`)
+}
 const jetsam = new Hono<{ Variables: {user: {id: string, table: string}} }>()
 
 jetsam.post('/bridge', async c => {
@@ -34,8 +43,21 @@ jetsam.post('/cost', c => {
   return c.json(billing.calculateSubpointForCargo(+size, downloads, retention))
 })
 
-jetsam.post('/start', verifyRequest(['sailor']), async c => {
-  
+jetsam.post('/start', async c => {
+  if(!endpoint){ throw new HTTPException(400, { message: 'The object storage endpoint was not found' }) }
+  const calcium = await fetch(endpoint+'/b2api/v4/b2_authorize_account', {
+    method: 'GET',
+    headers: {
+      'Authorization': `Basic ${keys()}`
+    }
+  })
+
+  if(!calcium.ok){
+    console.log(await calcium.json())
+  } else {
+    console.log(await calcium.json())
+  }
+  return c.json({returned: true})
 })
 
 export default jetsam
